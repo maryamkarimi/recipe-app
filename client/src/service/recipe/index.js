e4fdb1fec8bd8db5a483ef10aea0828440ef294a
Maryam Karimi <mkarimif@uwo.ca>
2020-11-06 16:36:14 -0500

    Updated the headers for getRecipeByIngredients to work with rapid API

--- client/src/service/recipe/index.js
+++ client/src/service/recipe/index.js
@@ -4,13 +4,15 @@ import { handleError, pullOutJson } from '../handlers'
 export const getRecipeByIngredientsService = (ingredientsList, recipeCount) => {
     const headers = new Headers({
         'content-type': 'application/json',
+        'x-rapidapi-host': getApiUrl(),
+        'x-rapidapi-key': getApiKey()
     })
 
     const ingredients = ingredientsList.join(',+')
-    const params = `ingredients=${ingredients}&number=${recipeCount}&apiKey=${getApiKey()}`
+    const params = `ingredients=${ingredients}&number=${recipeCount}`
 
     return fetch(
-            encodeURI(`${getApiUrl()}/findByIngredients?${params}`),
+            encodeURI(`https://${getApiUrl()}/recipes/findByIngredients?${params}`),
             {
                 method: 'GET',
                 headers,
