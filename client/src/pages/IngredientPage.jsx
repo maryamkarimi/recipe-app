b1f091f69d1a244a89538b4c6b9f51d2d6db298a
Joon Lee <joon.lee727@gmail.com>
2020-11-09 16:37:53 -0500

    Create Ingredients Page
    - Just adds basic Collapse component from antd
    - Adds basic styles from antd as well

--- client/src/pages/IngredientPage.jsx
+++ client/src/pages/IngredientPage.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { Collapse } from 'antd';
+
+const { Panel } = Collapse
+
+const testText = 'This is where the ingredients would go.';
 
 const IngredientPage = () => {
-  return <div>This is the ingredient selection page</div>;
+  // I'm just copy and pasting the example from antd here for now
+  return (
+    <div>
+      <Collapse
+        bordered={false}
+        defaultActiveKey={['1']}
+        className="site-collapse-custom-collapse"
+        ghost
+      >
+        <Panel header="This is panel header 1" key="1" className="site-collapse-custom-panel">
+          <p>{testText}</p>
+        </Panel>
+        <Panel header="This is panel header 2" key="2" className="site-collapse-custom-panel">
+          <p>{testText}</p>
+        </Panel>
+        <Panel header="This is panel header 3" key="3" className="site-collapse-custom-panel">
+          <p>{testText}</p>
+        </Panel>
+      </Collapse>
+    </div>
+  );
 };
 
 export default IngredientPage;
