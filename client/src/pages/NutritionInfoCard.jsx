027beca74a04b6b6482413abfa18bef449ff3eb5
Maryam Karimi <mkarimif@uwo.ca>
2020-11-29 21:09:37 -0500

    Removed unused imports

--- client/src/pages/NutritionInfoCard.jsx
+++ client/src/pages/NutritionInfoCard.jsx
@@ -1,12 +1,8 @@
-import React from "react";
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
@@ -187,7 +183,7 @@ const data = {
             "percentOfDailyNeeds": 1.53,
             "title": "Vitamin A"
         }
-    ]    
+    ]
 }
 
 const useStyles = makeStyles((theme) => ({
