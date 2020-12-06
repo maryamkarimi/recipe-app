b80d9461c81ae29821e93c019d32861979973d3f
Jerry Iu <jerryiu424@gmail.com>
2020-12-06 14:35:58 -0500

    🔢 Shows number of selected ingredients per category

--- client/src/components/IngredientSideBar.jsx
+++ client/src/components/IngredientSideBar.jsx
@@ -5,38 +5,59 @@ import categories from '../data/ingredientsByCategory.json';
 import FoodIcon from '../assets/icons/index';
 import './IngredientSideBar.less';
 
-const IngredientSideBar = ({ selectedIngredients, handleCheckboxChange }) => (
-  <Menu
-    selectedKeys={selectedIngredients}
-    mode="inline"
-    multiple
-    className="ingredients-menu"
-  >
-    {categories.map((category) => (
-      <Menu.SubMenu
-        key={category.category_name}
-        title={<span className="bold-text">{category.category_name}</span>}
-        icon={<FoodIcon category={category.category_name} />}
-      >
-        {category.ingredients.map((ingredient) => (
-          <Menu.Item key={ingredient.name} style={{ marginBottom: '-10px' }}>
-            <Checkbox
-              value={ingredient.name}
-              onChange={handleCheckboxChange}
-              checked={selectedIngredients.includes(ingredient.name)}
-              className="menu-item-checkbox"
-            >
-              {ingredient.name}
-            </Checkbox>
-          </Menu.Item>
-        ))}
-      </Menu.SubMenu>
-    ))}
-  </Menu>
-);
-
+const IngredientSideBar = ({
+  selectedIngredients,
+  selectedIngredientPerCategory,
+  handleCheckboxChange,
+}) => {
+  return (
+    <Menu
+      selectedKeys={selectedIngredients}
+      mode='inline'
+      multiple
+      className='ingredients-menu'>
+      {categories.map((category) => {
+        return (
+          <Menu.SubMenu
+            key={category.category_name}
+            title={
+              <span className='bold-text'>
+                {category.category_name}{' '}
+                {selectedIngredientPerCategory[category.category_name]}/
+                {category.ingredients.length}{' '}
+              </span>
+            }
+            icon={<FoodIcon category={category.category_name} />}>
+            {category.ingredients.map((ingredient) => {
+              return (
+                <Menu.Item
+                  key={ingredient.name}
+                  style={{ marginBottom: '-10px' }}>
+                  <Checkbox
+                    value={ingredient.name}
+                    onChange={(event) => {
+                      console.log('Event:', event);
+                      return handleCheckboxChange(
+                        category.category_name,
+                        event
+                      );
+                    }}
+                    checked={selectedIngredients.includes(ingredient.name)}
+                    className='menu-item-checkbox'>
+                    {ingredient.name}
+                  </Checkbox>
+                </Menu.Item>
+              );
+            })}
+          </Menu.SubMenu>
+        );
+      })}
+    </Menu>
+  );
+};
 IngredientSideBar.propTypes = {
   selectedIngredients: PropTypes.arrayOf(PropTypes.string).isRequired,
+  selectedIngredientPerCategory: PropTypes.object.isRequired,
   handleCheckboxChange: PropTypes.func.isRequired,
 };
 
