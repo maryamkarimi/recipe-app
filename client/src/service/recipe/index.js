c394a0f1a2a29abba84824324c23471d7ffadf5a
Jerry Iu <jerryiu424@gmail.com>
2020-12-07 22:16:37 -0500

    Adds service to pull data from spoonacular

--- client/src/service/recipe/index.js
+++ client/src/service/recipe/index.js
@@ -1,40 +1,48 @@
-import { getApiKey, getApiUrl } from '../index'
-import { handleError, pullOutJson } from '../handlers'
+import { getApiKey, getApiUrl } from '../index';
+import { handleError, pullOutJson } from '../handlers';
+
+const headers = new Headers({
+  'content-type': 'application/json',
+});
 
 export const getRecipeByIngredientsService = (ingredientsList, recipeCount) => {
-    const headers = new Headers({
-        'content-type': 'application/json'
-    });
+  const ingredients = ingredientsList
+    .map((ingredients) => ingredients.replace(/\s/g, '+'))
+    .join(',+');
+  const params = `ingredients=${ingredients}&number=${recipeCount}`;
 
-    const ingredients = ingredientsList.map(ingredients => ingredients.replace(/\s/g, '+')).join(',+');
-    const params = `ingredients=${ingredients}&number=${recipeCount}`;
-
-    return fetch(
-        encodeURI(`https://${getApiUrl()}/recipes/findByIngredients?apiKey=${getApiKey()}&${params}`),
-        {
-            method: 'GET',
-            headers,
-        },
-    )
-        .then(handleError)
-        .then(pullOutJson);
-}
+  return fetch(
+    encodeURI(
+      `https://${getApiUrl()}/recipes/findByIngredients?apiKey=${getApiKey()}&${params}`
+    ),
+    { method: 'GET', headers }
+  )
+    .then(handleError)
+    .then(pullOutJson);
+};
 
 // https://spoonacular.com/food-api/docs#Search-Recipes-Complex
 export const getRecipeByBasicSearchService = (searchTerm, recipeCount) => {
-    const headers = new Headers({
-        'content-type': 'application/json'
-    });
+  const queryTerms = searchTerm.replace(/\s/g, '+');
+  const params = `query=${queryTerms}&number=${recipeCount}`;
 
-    const params = `query=${searchTerm.replace(/\s/g, '+')}&number=${recipeCount}`
+  return fetch(
+    encodeURI(
+      `https://${getApiUrl()}/recipes/complexSearch?apiKey=${getApiKey()}&${params}`
+    ),
+    { method: 'GET', headers }
+  )
+    .then(handleError)
+    .then(pullOutJson);
+};
 
-    return fetch(
-        encodeURI(`https://${getApiUrl()}/recipes/complexSearch?apiKey=${getApiKey()}&${params}`),
-        {
-            method: 'GET',
-            headers
-        }
-    )
-        .then(handleError)
-        .then(pullOutJson);
-}
+export const getRecipeDetailByRecipeId = (id) => {
+  return fetch(
+    encodeURI(
+      `https://${getApiUrl()}/recipes/${id}/information?apiKey=${getApiKey()}&includeNutrition=true`
+    ),
+    { method: 'GET', headers }
+  )
+    .then(handleError)
+    .then(pullOutJson);
+};
